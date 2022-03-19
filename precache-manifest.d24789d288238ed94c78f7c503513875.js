ffb789341c75572e9973d1b3b48b2da43e059c14
Hashmat Ali <shahhashmatali667@gmail.com>
2022-03-20 00:41:25 +0500

    Updates

---
+++ precache-manifest.d24789d288238ed94c78f7c503513875.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba65ef350d32c584a717984f25bfdea9",
+    "url": "/liveWeatherApp/index.html"
+  },
+  {
+    "revision": "9ab5639dff1032d9efbd",
+    "url": "/liveWeatherApp/static/css/2.ba238490.chunk.css"
+  },
+  {
+    "revision": "8c3abcaa75da73df4149",
+    "url": "/liveWeatherApp/static/css/main.f21e9769.chunk.css"
+  },
+  {
+    "revision": "9ab5639dff1032d9efbd",
+    "url": "/liveWeatherApp/static/js/2.da359ce6.chunk.js"
+  },
+  {
+    "revision": "8c3abcaa75da73df4149",
+    "url": "/liveWeatherApp/static/js/main.4ce83d4a.chunk.js"
+  },
+  {
+    "revision": "8df58a26e38ed52ba8b8",
+    "url": "/liveWeatherApp/static/js/runtime~main.ce5eac24.js"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/liveWeatherApp/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/liveWeatherApp/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/liveWeatherApp/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/liveWeatherApp/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
